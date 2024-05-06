1bf7683296468913687d2ab8469fb4d7b52d6abf
Agatha-Moreira <1agatha.m.f@gmail.com>
2024-05-06 15:00:32 -0300

    feat: Exemplo Js

---
+++ exemplo.js
@@ -0,0 +1,24 @@
+// Escrevs um programa que solicite o nome do usuário e lhe de boas-vindas,
+// exibindo uma mensagem pelo console.
+
+// prompt -> Recebe uma mensagem digitada
+// let nome = prompt("Olá, digite seu nome:")
+
+//  Concatenação
+// console.log("Seja bem vindo " + nome + "!"); 
+
+let pontos = 100
+
+// parseInt -> Transforma o que está entre () em número inteiro.
+// let bonus = parseInt( prompt("Quantos pontos você deseja somar?"))
+
+console.log(pontos + bonus)
+
+// parseFloat() -> Transforma o que está entre () em numero com , (quebrado)
+let notaAluno = parseFloat(prompt("Digite a nota do aluno:"))
+
+console.log(notaAluno)
+
+
+// == -> verifica se um valor é igual á outro
+// === -> verifica se um valor e o tipo dos itens são iguais
